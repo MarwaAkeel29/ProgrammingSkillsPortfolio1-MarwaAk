c792def07b64398a183a94794d4c8009d361e947
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-16 19:59:24 +0400

    Create Exercise1 Coding is Cool.py

---
+++ Exercise1 Coding is Cool.py
@@ -0,0 +1,9 @@
+#Exercise 1: Coding is Cool
+
+word1 = ("Coding ")
+word2 = ("is ")
+word3 = ("Cool ")
+
+#using string concatenation to combine the declared variables and printing the phrase.
+phrase = (word1+word2+word3)
+print(phrase)

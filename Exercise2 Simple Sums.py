ab853aea2ef0c78d0ca58bfb11d52a506589257c
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-16 20:11:40 +0400

    Create Exercise2 Simple Sums.py

---
+++ Exercise2 Simple Sums.py
@@ -0,0 +1,8 @@
+#Exercise 2: Simple Sums
+
+a = 8  #Declaring a variable "a" and assigning the value 8 to it.
+b = 10  ##Declaring a variable "b" and assigning the value 10 to it.
+
+sum = (a+b) #Declaring a third variable that stores the sum of first variables.
+
+print("The value of the sum a and b is", sum) #prints the sum of the first and second variable.

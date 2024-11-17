32116ad448752a33af55e34d9431878c5d905411
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-17 16:39:52 +0400

    Create Exercise4 Primitive Quiz.py

---
+++ Exercise4 Primitive Quiz.py
@@ -0,0 +1,94 @@
+#Exercise 4: Primitive Quiz
+
+Question = input("What is the capital of France? ")
+if Question.lower()== "Paris".lower():
+    print("Your answer is CORRECT","Well done!")
+else:
+    print("Your answer is WRONG!")    
+
+
+#Advanced Requirements:
+name = (input("Enter your name here! "))
+print("Hello!",name, " Welcome to a small quizz,","let's Answer the Capitals of 10 Europian Countries") 
+
+
+print("Question 1") 
+
+Question1 = input("What is the capital of United Kingdom? ")
+if Question1.lower()== "london".lower():
+    print("Your answer is CORRECT","Impressive")
+else:
+    print("Your answer is WRONG!") 
+
+print("Question 2")
+
+Question2 = input("What is the capital of Italy? ")
+if Question2.lower()== "Rome".lower():
+    print("Your answer is CORRECT","Amazing")
+else:
+    print("Your answer is WRONG!", "keep going!")
+
+print("Question 3") 
+
+Question3 = input("What is the capital of Spain? ")
+if Question3.lower()== "Madrid".lower():
+    print("Your answer is CORRECT","fantastic")
+else:
+    print("Your answer is WRONG!", "You can still do better")
+
+print("Question 4")    
+
+Question4 = input("What is the capital of Germany? ")
+if Question4.lower()== "Berlin".lower():
+    print("Your answer is CORRECT","Well done")
+else:
+    print("Your answer is WRONG!", "Its never too late")
+
+print("Question 5")   
+
+Question5 = input("What is the capital of Russia? ")
+if Question5.lower()== "Moscow".lower():
+    print("Your answer is CORRECT","superb")
+else:
+    print("Your answer is WRONG!","Better luck next time!")
+
+print("Question 6")  
+
+Question6 = input("What is the capital of Switzerland? ")
+if Question6.lower()== "Bern".lower():
+    print("Your answer is CORRECT","Hats off")
+else:
+    print("Your answer is WRONG!","keep going!")
+
+print("Question 7")   
+
+Question7 = input("What is the capital of Bulgaria? ")
+if Question7.lower()== "Sofia".lower():
+    print("Your answer is CORRECT","hurray!")
+else:
+    print("Your answer is WRONG!","you can do it commom!")
+
+print("Question 8")  
+
+Question8 = input("What is the capital of Norway? ")
+if Question8.lower()== "Oslo".lower():
+    print("Your answer is CORRECT","2 more to go!")
+else:
+    print("Your answer is WRONG!","keep going")
+
+print("Question 9")  
+
+Question9 = input("What is the capital of Ireland? ")
+if Question9.lower()== "Dublin".lower():
+    print("Your answer is CORRECT","awesome!")
+else:
+    print("Your answer is WRONG!", "Almost there!")
+
+print("Question 10")   
+
+Question10 = input("What is the capital of Finland? ")
+if Question10.lower()== "Helsinki".lower():
+    print("Your answer is CORRECT","fabulous!")
+else:
+    print("Your answer is WRONG!","try again!")
+

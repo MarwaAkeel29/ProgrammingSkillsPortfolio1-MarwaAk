38e92ef996a34de75aaa2b6827e19b12704c9122
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-17 20:17:05 +0400

    Update Exercise2 Simple Sums.py
    comment added

--- Exercise2 Simple Sums.py
+++ Exercise2 Simple Sums.py
@@ -3,6 +3,6 @@
 a = 8  #Declaring a variable "a" and assigning the value 8 to it.
 b = 10  ##Declaring a variable "b" and assigning the value 10 to it.
 
-sum = (a+b) #Declaring a third variable that stores the sum of first variables.
+sum = (a+b) #Declaring a third variable that stores the sum of first and second variables.
 
 print("The value of the sum a and b is", sum) #prints the sum of the first and second variable.

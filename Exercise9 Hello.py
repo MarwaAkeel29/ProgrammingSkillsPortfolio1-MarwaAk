8cec2039d686b7d57fcfa4d8e6dc78778bc436d4
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-19 10:44:17 +0400

    Added Statements

---
+++ Exercise9 Hello.py
@@ -0,0 +1,11 @@
+#Exercise 9: Hello
+def hello():
+    print("Hello") #this will display the the string Hello
+
+def main():
+    hello() #Calling the  hello() function
+
+if __name__ == "__main__":
+    main()    
+    
+     

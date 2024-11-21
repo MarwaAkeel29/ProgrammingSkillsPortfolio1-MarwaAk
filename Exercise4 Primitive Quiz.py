167d41b7b883f64d118ead59676b59c61b510ce0
Marwa Akeel <marwaakeel2020@gmail.com>
2024-11-21 23:04:23 +0400

    Update Exercise4 Primitive Quiz.py

--- Exercise4 Primitive Quiz.py
+++ Exercise4 Primitive Quiz.py
@@ -1,23 +1,22 @@
 #Exercise 4: Primitive Quiz
 
-Question = input("What is the capital of France? ")
-if Question.lower()== "Paris".lower():
-    print("Your answer is CORRECT,","Well done!")
+Question = input("What is the capital of France? ") #asking user to input the capital of France
+if Question.lower()== "Paris".lower(): #using if-else statement as there is more than one condition 
+    print("Your answer is CORRECT","Well done!")
 else:
     print("Your answer is WRONG!")    
 
 
 #Advanced Requirements:
-name = (input("Enter your name here! "))
+name = (input("Enter your name here! ")) #asking user to input their name.
 print("Hello!",name, " Welcome to a small quizz,","let's Answer the Capitals of 10 Europian Countries") 
-points = 0
+
 
 print("Question 1") 
 
 Question1 = input("What is the capital of United Kingdom? ")
-if Question1.lower()== "london".lower():
-    print("Your answer is CORRECT,","Impressive!")
-    points += 1
+if Question1.lower()== "london".lower(): #using .lower() so that all upper case will be converted to lower case
+    print("Your answer is CORRECT","Impressive")
 else:
     print("Your answer is WRONG!") 
 
@@ -25,8 +24,7 @@ print("Question 2")
 
 Question2 = input("What is the capital of Italy? ")
 if Question2.lower()== "Rome".lower():
-    print("Your answer is CORRECT,","Amazing!")
-    points += 1
+    print("Your answer is CORRECT","Amazing")
 else:
     print("Your answer is WRONG!", "keep going!")
 
@@ -34,26 +32,23 @@ print("Question 3")
 
 Question3 = input("What is the capital of Spain? ")
 if Question3.lower()== "Madrid".lower():
-    print("Your answer is CORRECT,","fantastic!")
-    points += 1
+    print("Your answer is CORRECT","fantastic")
 else:
-    print("Your answer is WRONG!", "You can still do better!")
+    print("Your answer is WRONG!", "You can still do better")
 
 print("Question 4")    
 
 Question4 = input("What is the capital of Germany? ")
 if Question4.lower()== "Berlin".lower():
-    print("Your answer is CORRECT,","Well done!")
-    points += 1
+    print("Your answer is CORRECT","Well done")
 else:
-    print("Your answer is WRONG!", "Its never too late!")
+    print("Your answer is WRONG!", "Its never too late")
 
 print("Question 5")   
 
 Question5 = input("What is the capital of Russia? ")
 if Question5.lower()== "Moscow".lower():
-    print("Your answer is CORRECT,","superb!")
-    points += 1
+    print("Your answer is CORRECT","superb")
 else:
     print("Your answer is WRONG!","Better luck next time!")
 
@@ -61,8 +56,7 @@ print("Question 6")
 
 Question6 = input("What is the capital of Switzerland? ")
 if Question6.lower()== "Bern".lower():
-    print("Your answer is CORRECT,","Hats off!")
-    points += 1
+    print("Your answer is CORRECT","Hats off")
 else:
     print("Your answer is WRONG!","keep going!")
 
@@ -70,8 +64,7 @@ print("Question 7")
 
 Question7 = input("What is the capital of Bulgaria? ")
 if Question7.lower()== "Sofia".lower():
-    print("Your answer is CORRECT,","hurray!")
-    points += 1
+    print("Your answer is CORRECT","hurray!")
 else:
     print("Your answer is WRONG!","you can do it commom!")
 
@@ -79,8 +72,7 @@ print("Question 8")
 
 Question8 = input("What is the capital of Norway? ")
 if Question8.lower()== "Oslo".lower():
-    print("Your answer is CORRECT,","2 more to go!")
-    points += 1
+    print("Your answer is CORRECT","2 more to go!")
 else:
     print("Your answer is WRONG!","keep going")
 
@@ -88,8 +80,7 @@ print("Question 9")
 
 Question9 = input("What is the capital of Ireland? ")
 if Question9.lower()== "Dublin".lower():
-    print("Your answer is CORRECT,","awesome!")
-    points += 1
+    print("Your answer is CORRECT","awesome!")
 else:
     print("Your answer is WRONG!", "Almost there!")
 
@@ -97,10 +88,6 @@ print("Question 10")
 
 Question10 = input("What is the capital of Finland? ")
 if Question10.lower()== "Helsinki".lower():
-    print("Your answer is CORRECT,","fabulous!")
-    points += 1
+    print("Your answer is CORRECT","fabulous!")
 else:
     print("Your answer is WRONG!","try again!")
-
-print = ("Your total points is",points/10)    
-
